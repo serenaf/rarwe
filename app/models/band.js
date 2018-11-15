40c4c7202ac07dd25421e91b45fe218557f4dfef
serenaf <serena@intercom.io>
2018-11-15 08:31:57 +0000

    test

--- app/models/band.js
+++ app/models/band.js
@@ -1,10 +1,9 @@
-import DS from 'ember-data';
-
+import DS from 'ember-data'
 
 const { Model, attr, hasMany } = DS;
 
 export default Model.extend({
   name: attr('string'),
   description: attr('string'),
-  songs: hasMany(),
+  songs: hasMany(), // No need to pass 'song', it's inferred from the key name
 });
--- app/models/song.js
+++ app/models/song.js
@@ -1,8 +1,9 @@
-import DS from 'ember-data';
+import DS from 'ember-data'
 
 const { Model, attr, belongsTo } = DS;
+
 export default Model.extend({
   title: attr('string'),
-  rating: attr('string'),
+  rating: attr('number'),
   band: belongsTo(),
 });
